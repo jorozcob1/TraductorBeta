5ccac238904b60791102241dcfa2c1055ffb2d05
Jose <Usuario@DESKTOP-0R7DNT9>
2020-05-27 15:20:28 -0600

    Final

---
+++ src/traductorbeta/TraductorBeta.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package traductorbeta;
+//import ManejoDeArchivos.Mover;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author Usuario
+ */
+public class TraductorBeta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+        interfaz inte=new interfaz();
+        inte.show();
+    
+    }
+    
+}
